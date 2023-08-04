c3d3ca63d7b1d73ed7a92c78596f9905656fbda3
blacksecrettoken <secretsuperdev92@gmail.com>
2023-08-04 13:50:08 -0700

    finish

--- main.py
+++ main.py
@@ -7,19 +7,38 @@ import time
 def main():
     driver = webdriver.Chrome()
     driver.maximize_window()
-    #user_input = input("Please enter something: ")
+    first = 0
+    user_input = input("Please enter something: ")
     try:
-        driver.get("https://www.ord.io/21316151")
-        connect_button = driver.find_elements(By.TAG_NAME, 'button')[2]
-        driver.execute_script("arguments[0].click();",connect_button)
-        time.sleep(1)
-        unisat_button = driver.find_elements(By.TAG_NAME, 'button')[18]
+        driver.get("https://www.ord.io/13058521")
+
+        for i in range(2):
+            connect_button = driver.find_elements(By.TAG_NAME, 'button')[2]
+            driver.execute_script("arguments[0].click();",connect_button)
+            time.sleep(1)
+            unisat_button = driver.find_elements(By.TAG_NAME, 'button')[18]
+
+            driver.execute_script("arguments[0].click();",unisat_button)
+            
+            if first == 0:
+                time.sleep(10)
+                first = 1
+            else:
+                time.sleep(3)
+            favorite_button = driver.find_elements(By.TAG_NAME, 'button')[6]
+            driver.execute_script("arguments[0].click();",favorite_button)
+            time.sleep(1)
+
+            address_button = driver.find_elements(By.TAG_NAME, 'button')[2]
+            driver.execute_script("arguments[0].click();",address_button)
+            time.sleep(1)
+            signout_button = driver.find_elements(By.TAG_NAME, 'button')[3]
+            driver.execute_script("arguments[0].click();",signout_button)
+            time.sleep(1)
+
         # for i in range(len(unisat_button)):
         #     print(i)
         #     print(unisat_button[i].text)
-        driver.execute_script("arguments[0].click();",unisat_button)
-        
-        #connect_button.click()
         user_input = input("Please enter something: ")
     except Exception as e:
         print(e)
